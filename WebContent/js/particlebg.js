9a8e7da853f45c764ddbbe998a0df21f10401c5b
DeepakShankar <Danivas!23>
2017-06-22 00:18:29 -0400

    UI changes for portfolio web page

--- WebContent/js/particlebg.js
+++ WebContent/js/particlebg.js
@@ -3,11 +3,11 @@
  * the animated background.
  */
 
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
 	particleground(document.getElementById('particlesBody'), {
 		dotColor : '#5cbdaa',
 		lineColor : '#5cbdaa'
 	});
 	var intro = document.getElementById('intro');
 	intro.style.marginTop = -intro.offsetHeight / 2 + 'px';
-}, false);
+}, false);*/
